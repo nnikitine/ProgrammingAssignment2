572288fcf8e6821761f501727ac2cb0eccdd2f4a
nnikitine <nikolai.nikitine@gmail.com>
2015-01-24 11:41:35 -0500

    updated to include the code
    Programming assignment 2 - caching and retrieving values

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Calculating and caching the value of an inverse of a matrix
 
-## Write a short comment describing this function
+## calculates a cached value of the inverse of the matrix in the parent environment
+## and creates a list with the caching and retrieval funtions
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m<-NULL
+        set<-function(y){
+                x<<-y
+                m<<-NULL
+        }
+        get<-function() x
+        setinverse <-function(solve) m<<-solve
+        getinverse<- function() m
+        list(set=set,get=get, setinverse=setinverse, getinverse=getinverse)
 }
 
 
-## Write a short comment describing this function
+## checks whether cached value of an inverse is available. Retrieves cached value if
+## it is available or calculated and caches it if not
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        m<-x$getinverse()
+        if(!is.null(m)){
+                message("getting cached data")
+                return(m)
+        }
+        data<-x$get()
+        m<-solve(data,...)
+        x$setinverse(m)
+        m
 }
